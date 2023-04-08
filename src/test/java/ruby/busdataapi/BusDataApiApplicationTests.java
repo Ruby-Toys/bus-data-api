0f9e5b6694c226f3c96bf4872361ac807f5dc6fe
MisterRuby <ruby8700@naver.com>
2023-04-08 23:23:27 +0900

    init - 프로젝트 생성

---
+++ src/test/java/ruby/busdataapi/BusDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ruby.busdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusDataApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
